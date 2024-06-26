41808774de4200cc7fc7bd8184ab4273b5730e60
Neto <j.altevirneto@gmail.com>
2024-06-26 11:50:19 -0300

    feat: começando o processo de potenciação com soma

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const prompt = require('prompt-sync')();
 
-let numero = +prompt("Informe um número que você deseja ver sua potência: ");
+let base = +prompt("Informe um número que você deseja ver sua potência: ");
 
 let expoente = +prompt("Agora digite um número para ser o expoente: ")
 let potencia;
 
 for(let i = 0; i < expoente; i++){
     for(let j = 0;j < expoente; j++){
-        potencia = numero + numero
+        potencia = base;
+        base = base;
+
     }
 }
 console.log(potencia)
